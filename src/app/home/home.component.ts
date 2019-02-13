0af8a66f62042036185e128ddec01ebdf03919a9
baburavi27 <baburavi27@gmail.com>
2019-02-13 16:59:15 +0530

    changes committed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DragulaService } from 'ng2-dragula';
 import 'hcl-ers-edge-responsive-table-master/src/core-grid-datatypes/core-grid-datatypes.js';
+
 declare var grapesjs: any; // Important!
+import thePlugin from 'grapesjs-plugin-export';
 
 @Component({
   selector: 'app-home',
@@ -32,20 +34,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.list = [];
-    this.apirequest = { "handlerURL": "https://my-json-server.typicode.com/VelmuruganHCL/gridDemo/db" };
-    this.tableconfig = {
-      "id": "materialsummary",
-      "theme": "blue",
-      "title": {
-        "text": "Employee Details",
-        "align": "center",
-        "style": { "fontSize": "18px", "fontWeight": "bold", "fontFamily": "Arial, Helvetica, sans-serif", "color": "#333333" }
-      },
-      "subtitle": { "text": "Employee details table", "align": "center", "style": { "fontSize": "14px", "fontFamily": "Arial, Helvetica, sans-serif", "fontWeight": "normal", "color": "#666666" } },
-      "search": { "enabled": true },
-      "cellTemplate": [{ "name": "checkbox", "value": "empid", "enabled": true }, { "name": "legend", "value": ["empid", "empname"], "color": ["#FFD042", "#db8f25"], "enabled": true }, { "name": "link", "value": ["empid"], "enabled": true }, { "name": "icon", "icons": ["edit", "delete", "download"], "enabled": true }]
-    };
-
+   
     const editor = grapesjs.init({
       // Indicate where to init the editor. You can also pass an HTMLElement
       container: '#gjs',
@@ -59,6 +48,15 @@ export class HomeComponent implements OnInit {
       panels: {
         defaults: []
       },
+
+      plugins: ['thePlugin'],
+      pluginsOpts: {
+        'thePlugin': {
+          addExportBtn: true,
+          btnLabel: 'Export to ZIP',
+          filenamePfx: 'grapesjs_template'
+        }
+      },
       blockManager: {
         appendTo: '#blocks',
         blocks: [{
@@ -68,9 +66,15 @@ export class HomeComponent implements OnInit {
             class: 'gjs-block-section'
           },
           content: {
-            script:  console.log("hello") ,
+            script: function () {
+              var script = document.createElement('script');
+              script.src = 'main.js';
+              document.body.appendChild(script);
+
+            },
             content: '<core-grid-datatypes apirequest={"handlerURL":"https://my-json-server.typicode.com/VelmuruganHCL/gridDemo/db"} tableconfig={"id":"materialsummary","theme":"blue"}></core-grid-datatypes>'
           }
+
         },
         {
           id: 'text',
@@ -102,7 +106,7 @@ export class HomeComponent implements OnInit {
         {
           id: 'export',
           className: 'btn-open-export',
-          label: 'Export Code',
+          label: 'Export',
           command: 'export-template',
           context: 'export-template' // For grouping context of buttons from the same panel
         }
